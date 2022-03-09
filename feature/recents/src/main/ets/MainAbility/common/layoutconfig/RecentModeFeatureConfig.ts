fcb57d0b84c36d886a961cad35b175ed1c837360
zhoujing <zhoujing96@huawei.com>
2022-03-09 16:50:03 +0800

    sync code form recents settings smartdock
    Signed-off-by: zhoujing <zhoujing96@huawei.com>

---
+++ feature/recents/src/main/ets/MainAbility/common/layoutconfig/RecentModeFeatureConfig.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import CommonConstants from '../../../../../../../../common/src/main/ets/default/constants/CommonConstants';
+import RecentsModeConfig from '../../../../../../../../common/src/main/ets/default/layoutconfig/RecentsModeConfig';
+
+/**
+ * Recent missions layout mode configuration
+ */
+export default class RecentModeFeatureConfig extends RecentsModeConfig {
+
+  protected constructor() {
+    super();
+  }
+
+  protected getPersistConfigJson(): string {
+    const persistConfig = JSON.parse(super.getPersistConfigJson());
+    return JSON.stringify(persistConfig);
+  }
+
+  /**
+   * Get the instance of recent missions layout mode configuration
+   */
+  static getInstance() {
+    if (globalThis.RecentModeFeatureConfigInstance == null) {
+      globalThis.RecentModeFeatureConfigInstance = new RecentModeFeatureConfig();
+      globalThis.RecentModeFeatureConfigInstance.initConfig();
+    }
+    return globalThis.RecentModeFeatureConfigInstance;
+  }
+
+  initConfig(): void {
+    super.initConfig();
+  }
+
+  getConfigLevel(): string {
+    return CommonConstants.LAYOUT_CONFIG_LEVEL_FEATURE;
+  }
+}
