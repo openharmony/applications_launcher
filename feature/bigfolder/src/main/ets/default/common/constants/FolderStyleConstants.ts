78507eafc0a940f2bdffbbc80a830fad7ed77c37
elizhouyf <zhouyufeng8@huawei.com>
2022-03-09 16:45:58 +0800

    appcenter bigfolder code sync
    Signed-off-by: elizhouyf <zhouyufeng8@huawei.com>

---
+++ feature/bigfolder/src/main/ets/default/common/constants/FolderStyleConstants.ts
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2021 Huawei Device Co., Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+export default class FolderStyleConstants {
+
+  // image resources
+  static readonly DEFAULT_ICON: any = $r('app.media.icon');
+  static readonly DEFAULT_BACKGROUND_IMAGE = '$media:ic_wallpaper_folder';
+  static readonly DEFAULT_ADD_FOLDER_APP_IMAGE = '/common/pics/ic_public_add.svg';
+
+  // font style resources
+  static readonly DEFAULT_FONT_COLOR = '#ffffff';
+  static readonly FOLDER_ADD_APP_FONT_COLOR = '#000000';
+
+  // layout percentage adaptation resources
+  static readonly PERCENTAGE_100 = '100%';
+  static readonly PERCENTAGE_80 = '80%';
+  static readonly PERCENTAGE_50 = '50%';
+
+  // image size resources
+  static readonly DEFAULT_APP_NAME_SIZE= 20;
+  static readonly DEFAULT_FOLDER_ADD_APP_NAME_SIZE= 16;
+  static readonly DEFAULT_APP_ITEM_WIDTH = 70;
+  static readonly DEFAULT_APP_ITEM_HEIGHT = 95;
+  static readonly DEFAULT_APP_ICON_SIZE_WIDTH = 70;
+  static readonly DEFAULT_APP_NAME_HEIGHT = 25;
+  static readonly DEFAULT_ADD_APP_ICON_SIZE = 50;
+  static readonly DEFAULT_ADD_APP_SIZE = 65;
+  static readonly DEFAULT_VP_30 = 30;
+  static readonly DEFAULT_OPEN_FOLDER_NAME_SIZE = 30;
+  static readonly DEFAULT_OPEN_FOLDER_TITLE_HEIGHT = 130;
+
+  // Folder add app grid list
+  static readonly DEFAULT_FOLDER_APP_DIALOG_LIST = 536;
+  static readonly DEFAULT_FOLDER_APP_GRID_LIST = 424;
+  static readonly DEFAULT_FOLDER_APP_GRID_MARGIN = 10;
+  static readonly DEFAULT_APP_GRID_TOGGLE_SIZE = 30;
+  static readonly DEFAULT_APP_GRID_TOGGLE_MARGIN = -30;
+  static readonly DEFAULT_APP_ADD_TEXT_MARGIN = 14;
+  static readonly DEFAULT_APP_ADD_TEXT_BOTTOM_MARGIN = 20;
+  static readonly DEFAULT_APP_ADD_WIDTH = '70%';
+  static readonly DEFAULT_APP_GRID_LIST_WIDTH = 360;
+  static readonly DEFAULT_APP_GRID_TEXT_FONT_SIZE = 20;
+  static readonly DEFAULT_APP_GRID_BUTTON_SIZE = 60;
+  static readonly DEFAULT_APP_GRID_RADIUS = 24;
+  static readonly DEFAULT_APP_ADD_TITLE_SIZE = 56;
+  static readonly DEFAULT_APP_ADD_DIALOG_WIDTH = 394;
+  static readonly DEFAULT_APP_ADD_DIALOG_HEIGHT = 536;
+  static readonly DEFAULT_BUTTON_HEIGHT = 40;
+  static readonly DEFAULT_DIALOG_BOTTOM_MARGIN = 16;
+}
