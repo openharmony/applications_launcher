d4635e86b04c5aabdfca3936b50ea926a70af478
zhoujing <zhoujing96@huawei.com>
2022-03-09 17:21:19 +0800

    remove build
    Signed-off-by: zhoujing <zhoujing96@huawei.com>

--- feature/settings/build/intermediates/js/release/ark/rich/assets/js/AbilityStage.js
+++
@@ -1,2 +0,0 @@
-(()=>{"use strict";({723:function(i,t){this&&this.__importDefault;Object.defineProperty(t,"__esModule",{value:!0});var e=globalThis.requireNapi("application.AbilityStage")||(isSystemplugin("application.AbilityStage","ohos")?globalThis.ohosplugin.application.AbilityStage:isSystemplugin("application.AbilityStage","system")?globalThis.systemplugin.application.AbilityStage:void 0);globalThis.exports.default=class extends e{onCreate(){console.log("settings MyAbilityStage onCreate is called")}}}})[723](0,{})})();
-//# sourceMappingURL=AbilityStage.js.map
