e8deb8f7f1b760773742b8b930da0fcee4be237c
zhengyongjie <15531316327@163.com>
2024-04-10 11:34:15 +0800

    “回退黑屏” 
    Signed-off-by: zhengyongjie <15531316327@163.com>

--- common/src/main/ets/default/layoutconfig/PageDesktopLayoutConfig.ts
+++ common/src/main/ets/default/layoutconfig/PageDesktopLayoutConfig.ts
@@ -18,7 +18,6 @@ import { ILayoutConfig } from './ILayoutConfig';
 import { CommonConstants } from '../constants/CommonConstants';
 import FileUtils from '../utils/FileUtils';
 import { GridLayoutInfo } from '../interface';
-import { PreferencesUtil } from '../manager/PreferencesUtil';
 
 const TAG = 'PageDesktopLayoutConfig';
 
@@ -134,10 +133,7 @@ export class PageDesktopLayoutConfig extends ILayoutConfig {
     }
     const configFromRdb = await globalThis.RdbStoreManagerInstance.queryGridLayoutInfo();
     if (configFromRdb) {
-      PreferencesUtil.getInstance().put('isFirstPowerOn', false);
       defaultConfig.layoutInfo = configFromRdb;
-    }else {
-      PreferencesUtil.getInstance().put('isFirstPowerOn', true);
     }
     this.mGridLayoutInfo = defaultConfig;
   }
